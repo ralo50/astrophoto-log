8c218c15642448e1bb9fe0818213f464af6e6bb9
Luka <ralo.luka@gmail.com>
2019-01-22 19:01:15 +0100

    Initial commit

---
+++ app/src/main/java/com/ralo/astrophotolog/Activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ralo.astrophotolog.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ralo.astrophotolog.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
