d471445f1c617573a21423316dab3edd3412d056
ralo50 <ekimekim1>
2019-01-31 19:52:34 +0100

    navigation drawer setup

--- app/src/main/java/com/ralo/astrophotolog/Activities/MainActivity.kt
+++ app/src/main/java/com/ralo/astrophotolog/Activities/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.ralo.astrophotolog.Activities
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import android.widget.TextView
 import com.ralo.astrophotolog.R
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +22,27 @@ class MainActivity : BaseActivity() {
 
     private fun setViews() {
         val nameText = findViewById<TextView>(R.id.textView)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.nav_open_drawer, R.string.nav_close_drawer)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
         nameText.setText("KOTLIN BRAT - DIZNILEND")
+    }
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
